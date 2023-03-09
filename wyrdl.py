3a2d15fa56750d453c771717be1a4e4a390c5ab8
kyle <j.kyle.omalley@gmail.com>
2023-03-08 22:43:33 -0500

    Finish part 1

---
+++ wyrdl.py
@@ -0,0 +1,20 @@
+# wydrl.py
+
+WORD = "SNAKE"
+
+for guess_num in range(1, 7):
+    guess = input(f"\nGuess {guess_num}: ").upper()
+    if guess == WORD:
+        print("Correct")
+        break
+    
+    correct_letters = {
+        letter for letter, correct in zip(guess, WORD) if letter == correct
+    }
+    misplaced_letterrs = set(guess) & set(WORD) - correct_letters
+    wrong_letters = set(guess) - set(WORD)
+    
+    print("Correct letters:", ", ".join(sorted(correct_letters)))
+    print("Misplaced letters:", ", ".join(sorted(misplaced_letterrs)))
+    print("Wrong letters:", ", ".join(sorted(wrong_letters)))
+    
