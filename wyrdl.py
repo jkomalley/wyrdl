95485c16f4a7682ac91bc11ffecbb3c4f5682094
kyle <j.kyle.omalley@gmail.com>
2023-03-13 22:06:08 -0400

    Finish part 2

--- wyrdl.py
+++ wyrdl.py
@@ -1,20 +1,33 @@
 # wydrl.py
+import pathlib
+import random
+from string import ascii_letters
 
-WORD = "SNAKE"
+WORDLIST = [
+    word.upper()
+    for word in pathlib.Path("wordlist.txt").read_text(encoding='utf-8').split('\n')
+    if len(word) == 5 and all(letter in ascii_letters for letter in word)
+]
+
+word = random.choice(WORDLIST)
+
+print(word)
 
 for guess_num in range(1, 7):
     guess = input(f"\nGuess {guess_num}: ").upper()
-    if guess == WORD:
+    if guess == word:
         print("Correct")
         break
     
     correct_letters = {
-        letter for letter, correct in zip(guess, WORD) if letter == correct
+        letter for letter, correct in zip(guess, word) if letter == correct
     }
-    misplaced_letterrs = set(guess) & set(WORD) - correct_letters
-    wrong_letters = set(guess) - set(WORD)
+    misplaced_letterrs = set(guess) & set(word) - correct_letters
+    wrong_letters = set(guess) - set(word)
     
     print("Correct letters:", ", ".join(sorted(correct_letters)))
     print("Misplaced letters:", ", ".join(sorted(misplaced_letterrs)))
     print("Wrong letters:", ", ".join(sorted(wrong_letters)))
-    
+
+else:
+    print(f"The word was {word}")
