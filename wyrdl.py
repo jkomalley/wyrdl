864b465d544235bb7d90789e0d4591396a6c05c6
kyle <j.kyle.omalley@gmail.com>
2023-03-14 00:08:18 -0400

    Finish part 3 and part of 4

--- wyrdl.py
+++ wyrdl.py
@@ -2,32 +2,71 @@
 import pathlib
 import random
 from string import ascii_letters
+from rich.console import Console
+from rich.theme import Theme
 
-WORDLIST = [
-    word.upper()
-    for word in pathlib.Path("wordlist.txt").read_text(encoding='utf-8').split('\n')
-    if len(word) == 5 and all(letter in ascii_letters for letter in word)
-]
+console = Console(width=40, theme=Theme({"warning": "red on yellow"}))
 
-word = random.choice(WORDLIST)
+def refresh_page(headline):
+    console.clear()
+    console.rule(f"[bold blue]:leafy_green: {headline} :leafy_green:[/]\n")
 
-print(word)
+def get_random_word(word_list):
+    """Get a random five-letter word from a list of strings.
 
-for guess_num in range(1, 7):
-    guess = input(f"\nGuess {guess_num}: ").upper()
-    if guess == word:
-        print("Correct")
-        break
+    ## Example:
     
-    correct_letters = {
-        letter for letter, correct in zip(guess, word) if letter == correct
-    }
-    misplaced_letterrs = set(guess) & set(word) - correct_letters
-    wrong_letters = set(guess) - set(word)
-    
-    print("Correct letters:", ", ".join(sorted(correct_letters)))
-    print("Misplaced letters:", ", ".join(sorted(misplaced_letterrs)))
-    print("Wrong letters:", ", ".join(sorted(wrong_letters)))
+    >>> get_random_word(["snake", "worm", "it'll"])
+    'SNAKE'
+    """
+    words = [
+        word.upper()
+        for word in word_list
+        if len(word) == 5 and all(letter in ascii_letters for letter in word)
+    ]
 
-else:
+    return random.choice(words)
+
+def show_guesses(guesses, word):
+    for guess in guesses:
+        styled_guess = []
+        for letter, correct in zip(guess, word):
+            if letter == correct:
+                style = "bold white on green"
+            elif letter in word:
+                style = "bold white on yellow"
+            elif letter in ascii_letters:
+                style = "white on #666666"
+            else:
+                style = "dim"
+            styled_guess.append(f"[{style}]{letter}[/]")
+            
+        console.print("".join(styled_guess), justify="center")
+
+def game_over(word):
     print(f"The word was {word}")
+
+def main():
+    # Pre-process
+    words_path = pathlib.Path(__file__).parent / "wordlist.txt"
+    words_list = words_path.read_text(encoding="utf-8").split("\n")
+    word = get_random_word(words_list)
+    guesses = ["_"*5] * 6
+
+    print(word)
+
+    # Process (main loop)
+    for i in range(6):
+        refresh_page(headline=f"Guess {i + 1}")
+        show_guesses(guesses, word)
+        
+        guesses[i] = input(f"\nGuess {i + 1}: ").upper()
+        if guesses[i] == word:
+            break
+
+    # Post-process
+    else:
+        game_over(word)
+
+if __name__ == "__main__":
+    main()
